8dec8ee438080ecc77698738dd0bb4b395442c81
wheatfox <wheatfox17@icloud.com>
2025-07-07 13:17:19 +0800

    Update eBPF program and submodule reference: Remove commented-out code in trace_write BPF program for clarity and update the submodule reference in linux-git to the latest commit.

--- tools/ebpf-programs/trace_write.bpf.c
+++ tools/ebpf-programs/trace_write.bpf.c
@@ -20,9 +20,6 @@ SEC("tp/syscalls/sys_enter_write")
 int handle_write(struct syscalls_enter_write_args *ctx) {
   pid_t pid = bpf_get_current_pid_tgid() >> 32;
   struct task_struct *task = (struct task_struct *)bpf_get_current_task_btf();
-  // if (bpf_strncmp(task->comm, TASK_COMM_LEN, "bpftool") == 0) {
-  //   return 0;
-  // }
   bpf_printk("BPF triggered sys_enter_write from PID %d\n", pid);
   return 0;
 }
